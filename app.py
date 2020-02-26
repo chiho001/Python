98acda6b1ac9221e369baa00150ea3b9557802be
CHIHO <chiho001@naver.com>
2020-02-26 16:20:22 +0900

    Initial commit

---
+++ app.py
@@ -0,0 +1,15 @@
+# coding = utf-8
+
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def home():
+    return render_template('index.html', subject="안녕하세요. 반갑습니다.")
+
+
+if __name__ == "__main__":
+    app.run()
